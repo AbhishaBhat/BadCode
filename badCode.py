e2c11c30e05d5d8acf328c31456904c9473b82de
AbhishaBhat <bhatabhisha@gmail.com>
2026-05-12 11:40:16 +0530

    Refactor badCode.py to improve code quality
    Refactor bad practices in badCode.py including removing hardcoded password, fixing duplicate code, and improving variable names.

--- badCode.py
+++ badCode.py
@@ -1,24 +1,20 @@
-# bad_ml.py
-
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import *
 
-# hardcoded path
+password = "admin123"
+
 df = pd.read_csv("C:/data/file.csv")
 
-# bad variable names
 a = df.drop("price", axis=1)
 b = df["price"]
 
-# duplicate code
 def fun(x):
     if x > 0:
         return x * 2
     else:
         return x * 2
 
-# empty exception
 model = LinearRegression()
 
 try:
@@ -26,11 +22,7 @@ try:
 except:
     pass
 
-# hardcoded password
-password = "admin123"
-
-# dead code
 if False:
-    print("never runs")
+    print("dead code")
 
-print("done")
+eval("print('hello')")
